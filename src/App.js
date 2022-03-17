9dea982b1026449aebe16cd3bc6abddbde230606
mariyasada <mariyasada2406@gmail.com>
2022-03-17 22:14:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MockMan from "mockman-js";
 import {Routes,Route} from "react-router";
 import Home from "./pages/Home/Home";
 import { ProductPage } from "./pages/ProductPage/ProductPage";
+import { SingleProductDetail } from "./pages/singleProductDetail/singleproductDetail";
 
 
 function App() {
@@ -13,7 +14,7 @@ function App() {
        <Nav/> 
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/productpage" element={<ProductPage/>}/>
+        <Route path="/productpage" element={<ProductPage/>}/>       
         <Route path="/mockman" element={<MockMan/>}/>
       </Routes>
       </div>
