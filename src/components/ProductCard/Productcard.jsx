b41b5d3cbdd32396ea706a515bcada3a7b19b76a
mariyasada <mariyasada2406@gmail.com>
2022-03-17 11:23:57 +0530

    destructuring the object as per suggestion

--- src/components/ProductCard/Productcard.jsx
+++ src/components/ProductCard/Productcard.jsx
@@ -5,33 +5,34 @@ import "../ProductCard/Productcard.css";
 
 
 export const Productcard = ({userProduct}) => {
+  const {image,isSoldOut,productName,brand,sellingPrice,originalPrice,discountOffer,isRatingGood,rating }=userProduct;
   return (
     <div className="card-for-ecommerce pd-1 card-with-shadow">
                 <div className="card-image-holder">
                   <Link to="">
-                    <img src={userProduct.image} className="card-image" alt="product-image" />
+                    <img src={image} className="card-image" alt="product-image" />
                   </Link>
-                  {userProduct.isSoldOut && (
+                  {isSoldOut && (
                     <span className="sold-out-badge">Sold out</span>
                   )}
                 </div>
 
                 <div className="card-body">
-                  <span className="card-title fw-300">{userProduct.productName}</span>
-                  <p className="card-detail">by {userProduct.brand}</p>
-                  <span className="discount"><FaRupeeSign className="rupees-sign"/> {userProduct.sellingPrice}</span>                  
-                  <span className="price"><FaRupeeSign className="rupees-sign" />{userProduct.originalPrice}</span>                 
-                  <p className="discountTag">({userProduct.discountOffer})</p>
+                  <span className="card-title fw-300">{productName}</span>
+                  <p className="card-detail">by {brand}</p>
+                  <span className="discount"><FaRupeeSign className="rupees-sign"/> {sellingPrice}</span>                  
+                  <span className="price"><FaRupeeSign className="rupees-sign" />{originalPrice}</span>                 
+                  <p className="discountTag">({discountOffer})</p>
                   <span className="rating-badge estee-badge">
                     <div
                       className={
-                        userProduct.isRatingGood
+                        isRatingGood
                           ? "rating-container bg-green border-round"
                           : "rating-container bg-yellow border-round"
                       }
                     >
                       <div className="rating-with-icon">
-                        {userProduct.rating}
+                        {rating}
                         <FaStar />
                       </div>
                     </div>
@@ -39,8 +40,8 @@ export const Productcard = ({userProduct}) => {
                   <div className="btns-in-flex"> 
                   <span>             
                       <Link to="/cart">
-                        <button className={userProduct.isSoldOut?"link-btn btn-lg btn-outline border-round cursor-drop":"link-btn btn-lg btn-outline border-round cursor-pointer "}>
-                           {userProduct.isSoldOut ? "NOTIFY ME" : "ADD TO CART"}
+                        <button className={isSoldOut?"link-btn btn-lg btn-outline border-round cursor-drop":"link-btn btn-lg btn-outline border-round cursor-pointer "}>
+                           {isSoldOut ? "NOTIFY ME" : "ADD TO CART"}
                         </button>
                       </Link>                       
                     </span>  
