7a5edbe3bb4b332a6308559305ea2ba67ef716a1
mariyasada <mariyasada2406@gmail.com>
2022-03-19 19:30:08 +0530

    feat: add wishlist page functionality to E-commerce

--- src/context/Reducer/WishListItemReducer.js
+++ src/context/Reducer/WishListItemReducer.js
@@ -4,7 +4,8 @@ export const WishListItemReducer=(state,action)=>{
     
     switch (action.type) {
         case "ADD_ITEM_TO_WISHLIST":
-            return {...state,  wishListItem:[...state.wishListItem,action.payload],wishListCount :state.wishListCount +1};
+            return {...state,  wishListItem:[...state.wishListItem,action.payload],
+                wishListCount :state.wishListCount +1};
 
         case "REMOVE_ITEM_FROM_WISHLIST":
             return {...state,wishListItem:state.wishListItem.filter((item)=>item._id!==action.payload._id),wishListCount :state.wishListCount -1}    
--- src/pages/WishListPage/WishListPage.jsx
+++ src/pages/WishListPage/WishListPage.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { Productcard, WishListCard } from "../../components/index";
-// import "../../components/ProductCard/Productcard.css";
+import {
+  ErrorMessage,
+  Productcard,
+  WishListCard,
+} from "../../components/index";
+import { useCartandWishList } from "../../context/CartAndWishlist-context";
 
 export const WishListPage = () => {
+  const { wishListState, wishListDispatch } = useCartandWishList();
+  const { wishListItem, wishListCount } = wishListState;
   return (
     <div className="main-content-container">
       <h2 className="heading">My WishList</h2>
       <div className="grid-3-column-layout">
-        <WishListCard />
-        <WishListCard />
-        <WishListCard />
-        <WishListCard />
+        {wishListItem.length === 0 ? (
+          <ErrorMessage message={"No Products in your WishList Page"} />
+        ) : (
+          wishListItem.map((product) => {
+            return <WishListCard product={product} key={product._id} />;
+          })
+        )}
       </div>
     </div>
   );
