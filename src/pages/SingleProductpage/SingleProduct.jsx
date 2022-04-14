cb3db325d09b3cc6f38602d3a11406cc1b760451
mariyasada <mariyasada2406@gmail.com>
2022-04-14 12:59:33 +0530

    Update SingleProduct.jsx

--- src/pages/SingleProductpage/SingleProduct.jsx
+++ src/pages/SingleProductpage/SingleProduct.jsx
@@ -7,14 +7,12 @@ import { useProduct } from "../../context/product-context";
 export const SingleProduct = () => {
   const { productList } = useProduct();
   const { productId } = useParams();
-  console.log(productId, "useparam");
 
   const getProductDetails = (products, productId) => {
     return products.find((product) => product._id === productId);
   };
 
   let product = getProductDetails(productList, productId);
-  console.log(product);
 
   return (
     <>
